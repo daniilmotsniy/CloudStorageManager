61f8fce2c2c6d4bc2392b3fade6419942e753ff2
Daniil_Motsnyi <daniil_motsnyi@epam.com>
2022-11-01 10:17:03 +0200

    chain added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import typing
 from uuid import uuid4
 from os import environ
 from pathlib import Path
@@ -8,12 +9,13 @@ import boto3
 import motor.motor_asyncio
 import uvicorn
 from bson import ObjectId
+from celery import chord
 from celery.result import AsyncResult
 from fastapi import FastAPI, UploadFile
 from starlette import status
 from starlette.responses import JSONResponse
 
-from worker import upload_to_s3
+from worker import upload_to_s3, remove_tmp_file
 
 app = FastAPI()
 
@@ -43,10 +45,10 @@ def get_status(task_id):
 
 
 @app.post('/upload_file/', status_code=201)
-async def upload_file(file: UploadFile, bucket_id: str, folder: str = None):
-    buckets = [bck async for bck in db.buckets.aggregate([
+async def upload_file(file: UploadFile, bucket_ids: typing.List[str], folder: str = None):
+    buckets = [bucket async for bucket in db.buckets.aggregate([
         {
-            '$match': {'_id': ObjectId(bucket_id)}
+            '$match': {'_id': {'$in': [ObjectId(bucket_id) for bucket_id in bucket_ids]}}
         },
         {
             '$project': {'folders': 1, 'name': 1, 'provider': 1}
@@ -60,29 +62,35 @@ async def upload_file(file: UploadFile, bucket_id: str, folder: str = None):
     ])]
     if not buckets:
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            content={'error': 'You have no such bucket!'})
+                            content={'error': 'You have no such bucket or folder!'})
 
+    Path('tmp').mkdir(parents=True, exist_ok=True)
+    tmp_file_path = f'tmp/{uuid4()}-{file.filename}'
+    async with aiofiles.open(tmp_file_path, 'wb') as out_file:
+        content = await file.read()
+        content_result = await out_file.write(content)
+
+    tasks = list()
     for db_bucket in buckets:
-        Path('tmp').mkdir(parents=True, exist_ok=True)
-        tmp_file_path = f'tmp/{uuid4()}-{file.filename}'
-        async with aiofiles.open(tmp_file_path, 'wb') as out_file:
-            content = await file.read()
-            content_result = await out_file.write(content)
-            await db.buckets.update_one(
-                {'_id': ObjectId(bucket_id)},
-                {
-                    '$push': {
-                        'files': {'name': file.filename, 'folder': folder}
-                    }
+        await db.buckets.update_one(
+            {'_id': ObjectId(db_bucket['_id'])},
+            {
+                '$push': {
+                    'files': {'name': file.filename, 'folder': folder}
                 }
-            )
-            if content_result:
-                if db_bucket['provider'] == 'aws':
-                    task = upload_to_s3.delay(tmp_file_path, db_bucket['name'], file.filename)
-                else:
-                    return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                        content={'error': 'Unknown provider in bucket!'})
-    return {'task_id': task.id}
+            }
+        )
+        if content_result:
+            if db_bucket['provider'] == 'aws':
+                tasks.append(upload_to_s3.si(tmp_file_path, db_bucket['name'], file.filename))
+            elif db_bucket['provider'] == 'gcp':
+                pass
+            else:
+                return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                    content={'error': 'Unknown provider in bucket!'})
+
+    upload_workflow = chord(*tasks, remove_tmp_file.si(tmp_file_path)).apply_async()
+    return {'task_id': upload_workflow.id}
 
 
 @app.post('/folders', response_description='Add new folder')
--- worker.py
+++ worker.py
@@ -1,8 +1,9 @@
 import os
+import logging
+import sys
 
 import boto3
 from celery import Celery
-
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
@@ -13,12 +14,21 @@ session = boto3.Session(region_name=os.environ.get('AWS_REGION', 'us-east-1'),
                         aws_secret_access_key=os.environ['AWS_SECRET_KEY'])
 client = session.client('s3')
 
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
 
 @celery.task(name="upload_to_s3")
 def upload_to_s3(file_path, bucket, path):
     try:
         client.upload_file(file_path, bucket, path)
+    except FileNotFoundError(file_path) as e:
+        logging.error(e)
+    return True
+
+
+@celery.task(name="remove_tmp_file")
+def remove_tmp_file(file_path):
+    try:
         os.remove(file_path)
     except FileNotFoundError(file_path) as e:
-        print(e)
-    return True
+        logging.error(e)
