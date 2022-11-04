096b7b8af94b202b24b26ea03ca0d0513e91b5f7
Daniil_Motsnyi <daniil_motsnyi@epam.com>
2022-11-04 23:19:37 +0200

    rm

--- app/server.py
+++ app/server.py
@@ -12,7 +12,3 @@ app.include_router(tasks_router)
 @app.get('/')
 async def root():
     return {'message': 'Welcome to CloudStorageManager!'}
-
-
-# if __name__ == '__main__':
-#     uvicorn.run('app:app', host='localhost', port=8080, reload=True)
